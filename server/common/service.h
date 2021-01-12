7d4a02c38dac5b6f8de34aed37a2e112fd6d001a
sobolanescu <38504992+sobolanescu@users.noreply.github.com>
2021-01-12 08:48:51 +0200

    Update service.h

--- server/common/service.h
+++ server/common/service.h
@@ -3,9 +3,9 @@ Add:
 #define ENABLE_DROP_EVENT
 #ifdef ENABLE_DROP_EVENT
   #define ENABLE_DROP_EVENT_MAX_DIFFERENCE  30 // maximum level difference
-#endif
-#define ENABLE_MOONLIGHT_CHEST_DROP
-#ifdef ENABLE_MOONLIGHT_CHEST_DROP
-  #define MOONLIGHT_CHEST_DROP_VNUM 50011 // item vnum
-  #define MOONLIGHT_CHEST_DROP_CHANCE 1 // chances to drop %
+  #define ENABLE_MOONLIGHT_CHEST_DROP
+    #ifdef ENABLE_MOONLIGHT_CHEST_DROP
+      #define MOONLIGHT_CHEST_DROP_VNUM 50011 // item vnum
+      #define MOONLIGHT_CHEST_DROP_CHANCE 1 // chances to drop %
+    #endif
 #endif
